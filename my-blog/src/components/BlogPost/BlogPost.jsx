84afeb956ec9e508f53a68a4260f9fb798fd46a8
Julius Sasmito <jujubeege@gmail.com>
2024-11-13 16:23:48 -0500

    Updated the blogPost

--- my-blog/src/components/BlogPost/BlogPost.jsx
+++ my-blog/src/components/BlogPost/BlogPost.jsx
@@ -1,8 +1,29 @@
+import { useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
 //import './BlogPost.css';
 import styles from './BlogPost.module.css';
+import LikeButton from '../LikeButton/LikeButton';
+import CommentSection from '../CommentSection/CommentSection';
+import { calculateReadTime } from '../../utils/readTime';
 
-function BlogPost({ title, content, author, date, readTime }) {
+
+function BlogPost({ id, title, content, author, date }) {
+  const [isExpanded, setIsExpanded] = useState(false);
+
+  const [readTime, setReadTime] = useState(0)
+
+  useEffect(() => {
+    setReadTime(calculateReadTime(content));
+  }, [content]);
+
+  const toggleContent = () => {
+    setIsExpanded(prev => !prev);
+  };
+
+  const displayContent = isExpanded 
+  ? content 
+  : content.slice(0, 200) + (content.length > 200 ? '...' : '');
+
   return (
     <article className={styles.blogPost}>
       <div className={styles.header}>
@@ -14,14 +35,25 @@ function BlogPost({ title, content, author, date, readTime }) {
         </div>
       </div>
       
-      <div className={styles.content}>
-        {content}
+      <div className={styles.blogPostContent}>
+        <p>{displayContent}</p>
+        {content.length > 200 && (
+          <button onClick={toggleContent} className= 'blogPostExpand'>
+            {isExpanded ? 'Read Less' : 'Read More'}
+          </button>
+        )}
+      </div>
+
+      <div className={styles.blogPostAction}>
+        <LikeButton initialLikes={0} />
+        <CommentSection postId={id} />
       </div>
     </article>
   );
 }
 
 BlogPost.propTypes = {
+  id: PropTypes.string.required,
   title: PropTypes.string.required,
   content: PropTypes.string.required,
   author: PropTypes.string.required,
