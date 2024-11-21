57b85f97f3c157d22b0bea5aa59d73293ca82aa9
Julius Sasmito <jujubeege@gmail.com>
2024-11-21 11:09:03 -0500

    Assignment_4 submit

--- my-blog/src/data/posts.js
+++ my-blog/src/data/posts.js
@@ -5,7 +5,9 @@ export const posts = [
       content: "Nintedno makes the best video games, I want the Switch 2 to have a mic WHEN IS IT COMING!?",
       author: "Jane Doe",
       date: "2024-03-15 ",
-      readTime: 5
+      readTime: 5,
+      category: "Gaming",
+      tags: ["Nintento", "switch"],
     },
     {
       id: 2,
@@ -13,7 +15,9 @@ export const posts = [
       content: "Props are the way we pass data between React components...",
       author: "John Smith",
       date: "2024-03-16 ",
-      readTime: 3
+      readTime: 3,
+      category: "Gaming",
+      tags: ["Coding", "switch"],
     },
     {
       id: 3,
@@ -21,15 +25,60 @@ export const posts = [
       content: "Props are the way we pass data between React components...",
       author: "Wandering Puppy",
       date: "2024-03-16 ",
-      readTime: 3
+      readTime: 3,
+      category: "Gaming",
+      tags: ["Props", "switch"],
     },
     {
       id: 4,
+      title: "Pokemon",
+      content: "Pokémon[a][b] is a Japanese media franchise consisting of video games, animated series and films, a trading card game, and other related media. The franchise takes place in a shared universe in which humans co-exist with creatures known as Pokémon, a large variety of species endowed with special powers. The franchise's target audience is children aged 5 to 12,[2] but it is known to attract people of all ages.[I]The franchise originated as a pair of role-playing games developed by Game Freak, from an original concept by its founder, Satoshi Tajiri. Released on the Game Boy on February 27, 1996, the games became sleeper hits and were followed by manga series, a trading card game, and anime series and films. From 1998 to 2000, Pokémon was exported to the rest of the world, creating an unprecedented global phenomenon dubbed 'pokemania'. By 2002, the craze had ended, after which Pokémon became a fixture in popular culture, with new products being released to this day. In the summer of 2016, the franchise spawned a second craze with the release of Pokémon Go, an augmented reality game developed by Niantic. Pokémon has since been estimated to be the world's highest-grossing media franchise and one of the best-selling video game franchises.",
+      author: "Oak",
+      date: "2024-03-16 ",
+      readTime: 3,
+      category: "Gaming",
+      tags: ["Pokemon"],
+    },
+
+    {
+      id: 5,
+      title: "Nintendos WHERE IS THE NEWS!!",
+      content: "Nintedno makes the best video games, I want the Switch 2 to have a mic WHEN IS IT COMING!?",
+      author: "Jane Doe",
+      date: "2024-03-15 ",
+      readTime: 5,
+      category: "Gaming",
+      tags: ["Nintento", "switch"],
+    },
+    {
+      id: 6,
       title: "Understanding Props",
       content: "Props are the way we pass data between React components...",
       author: "John Smith",
       date: "2024-03-16 ",
-      readTime: 3
+      readTime: 3,
+      category: "Gaming",
+      tags: ["Coding", "switch"],
+    },
+    {
+      id: 7,
+      title: "Understanding Props",
+      content: "Props are the way we pass data between React components...",
+      author: "Wandering Puppy",
+      date: "2024-03-16 ",
+      readTime: 3,
+      category: "Gaming",
+      tags: ["Props", "switch"],
+    },
+    {
+      id: 8,
+      title: "Understanding Props",
+      content: "Props are the way we pass data between React components...",
+      author: "John Smith",
+      date: "2024-03-16 ",
+      readTime: 3,
+      category: "Gaming",
+      tags: ["Nintento", "switch"],
     },
     // Add 1-2 more sample posts
   ];
